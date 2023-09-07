ca91c1d687f8c89e17373b773fe81eaad2ee440b
AbdelrahmanTarekMahmoud <AbdelrhmanTarekMahmoud@gmail.com>
2023-09-07 20:23:51 +0300

    Assignment1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,10 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+     const char * myName =  my_username(); //1
+     char * testName =malloc_username_from_conf_file(); //2
+     //TEST_ASSERT_EQUAL_STRING (expected, actual)
+     TEST_ASSERT_EQUAL_STRING (testName, myName); // 3
+
+    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
